f8b7706c7ee6a346e03dfc746c8a38227944d9fe
Dimitrii Vinogradov <siteddv@gmail.com>
2021-12-06 00:37:17 +0600

    added hidden bot password

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,38 +2,29 @@ package main
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/joho/godotenv"
+	"github.com/siteddv/pocketel_bot/pkg/telegram"
 	"log"
+	"os"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	botToken := os.Getenv("botToken")
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Panic(err)
 	}
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	telegramBot := telegram.NewBot(bot)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, _ := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil {
-			continue
-		}
-		// If we got a message
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		_, err := bot.Send(msg)
-		if err != nil {
-			log.Fatalf("Error during sending message: %s", err.Error())
-			return
-		}
+	if err = telegramBot.Start(); err != nil {
+		log.Fatalf("Error during starting bot")
 	}
 }
